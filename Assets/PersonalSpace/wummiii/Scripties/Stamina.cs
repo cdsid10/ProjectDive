90612474c213a573a9f179f169a14c47ba96b7ec
wummiii <73142482+wummiii@users.noreply.github.com>
2023-08-23 20:50:47 +0200

    Update Stamina.cs

--- Assets/PersonalSpace/wummiii/Scripties/Stamina.cs
+++ Assets/PersonalSpace/wummiii/Scripties/Stamina.cs
@@ -1,39 +1,76 @@
 using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Stamina : MonoBehaviour
 {
-#if ENABLE_INPUT_SYSTEM
-    private PlayerInput _playerInput;
-#endif
+    [SerializeField] private GameObject staminaText;
 
-    [SerializeField] private float maxStamina;
+    [SerializeField] private float _maxStamina;
+    [SerializeField] private float _increaseAmountPerSecond;
+    [SerializeField] private float _decreaseAmountPerSecond;
+    [SerializeField] private float _decreaseAmountPerJump;
+
+    private bool canJump = true;
+    private bool decreasingStamina = false;
+    private bool increasingStamina = false;
+
     private float currentStamina;
     private StarterAssetsInputs _input;
+
     // Start is called before the first frame update
     void Start()
     {
         _input = GetComponent<StarterAssetsInputs>();
-        currentStamina = maxStamina;
+        currentStamina = _maxStamina;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //doesnt check if the player acutally jumped, just if the key is pressed
-        if(/*_input.jump*/ Input.GetKeyDown(KeyCode.Space))
+        if (_input.jump && GetComponent<FirstPersonController>().Grounded && canJump)
         {
-            currentStamina -= 10;
+            canJump = false;
+            currentStamina -= _decreaseAmountPerJump;
+            StartCoroutine(Timer());
+        }
+        if (_input.sprint && decreasingStamina == false)
+        {
+            decreasingStamina = true;
+            StartCoroutine(DecreaseStaminaSprint());
+        }
+        if (currentStamina < _maxStamina && increasingStamina == false)
+        {
+            increasingStamina = true;
+            StartCoroutine(IncreaseStamina());
         }
 
-        //doesnt check if the player actually moves forward
-        if (_input.sprint)
-        {
-            currentStamina -= 0.1f;
-        }
-        Debug.Log(currentStamina);
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        staminaText.GetComponent<TextMeshProUGUI>().text = currentStamina.ToString();
+    }
+    private IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canJump = true;
+    }
+
+    private IEnumerator DecreaseStaminaSprint()
+    {
+        currentStamina -= _decreaseAmountPerSecond;
+        yield return new WaitForSeconds(0.5f);
+        decreasingStamina = false;
+    }
+    private IEnumerator IncreaseStamina()
+    {
+        currentStamina += _increaseAmountPerSecond;
+        yield return new WaitForSeconds(0.5f);
+        increasingStamina = false;
     }
 }
