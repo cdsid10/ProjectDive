92816354587483c8655e0c79acd4eaab0c148db1
wummiii <73142482+wummiii@users.noreply.github.com>
2023-08-24 05:36:09 +0200

    Update Stamina
    The script should kind of work now. Check it out

--- Assets/PersonalSpace/wummiii/Scripties/Stamina.cs
+++ Assets/PersonalSpace/wummiii/Scripties/Stamina.cs
@@ -1,77 +1,129 @@
 using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Stamina : MonoBehaviour
 {
     [SerializeField] private GameObject staminaText;
-
     [SerializeField] private float _maxStamina;
-    [SerializeField] private float _increaseAmountPerSecond;
-    [SerializeField] private float _decreaseAmountPerSecond;
-    [SerializeField] private float _decreaseAmountPerJump;
+    [SerializeField] private float _tickTime;
+    [SerializeField] private float _exhaustDuration;
+    [SerializeField] private float _staminaIncreasePerTick;
+    [SerializeField] private float _staminaDecreasePerTick;
+    [SerializeField] private float _staminaDecreasePerJump;
 
-    private bool canJump = true;
-    private bool decreasingStamina = false;
-    private bool increasingStamina = false;
+    private bool exhaustedDebuff = false;
+    private bool tickTimerRunning = false;
 
     private float currentStamina;
     private StarterAssetsInputs _input;
+    private FirstPersonController _firstPersonController;
 
-    // STILL NEED TO APPLY STUFF LIKE "CANT JUMP IF NO STAMINA OR CANT SPRINT, MAYBE ADD A SOME DELAY FOR REGENERATION AFTER PLAYER HIT 0 STAMINA
     // Start is called before the first frame update
     void Start()
     {
         _input = GetComponent<StarterAssetsInputs>();
+        _firstPersonController = GetComponent<FirstPersonController>();
         currentStamina = _maxStamina;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_input.jump && GetComponent<FirstPersonController>().Grounded && canJump)
+        // Check if we have enouugh stamina and take the firstPersonController's abilty to jump if we dont have enough
+        if(currentStamina < _staminaDecreasePerJump)
         {
-            canJump = false;
-            currentStamina -= _decreaseAmountPerJump;
-            StartCoroutine(Timer());
-        }
-        if (_input.sprint && decreasingStamina == false)
-        {
-            decreasingStamina = true;
-            StartCoroutine(DecreaseStaminaSprint());
-        }
-        if (currentStamina < _maxStamina && increasingStamina == false && decreasingStamina == false)
-        {
-            increasingStamina = true;
-            StartCoroutine(IncreaseStamina());
+            _firstPersonController.Grounded = false;
         }
 
+        // jump
+        if (_input.jump && _firstPersonController.Grounded && tickTimerRunning == false)
+        {
+            tickTimerRunning = true;
+            DecreaseStamina(_staminaDecreasePerJump);
+            StartCoroutine(JumpTimer());
+        }
+
+        // sprint
+        else if (_input.sprint && _input.move != Vector2.zero && tickTimerRunning == false && currentStamina > 0)
+        {
+            tickTimerRunning = true;
+            DecreaseStamina(_staminaDecreasePerTick);
+            StartCoroutine(TickTimer());
+        }
+        // stamina regeneration
+        else if (currentStamina < _maxStamina && tickTimerRunning == false && exhaustedDebuff == false)
+        {
+            tickTimerRunning = true;
+            IncreaseStamina(_staminaIncreasePerTick);
+            StartCoroutine(TickTimer());
+        }
+
+        // exhaust if zero mana, activate/deactivate sprint ability by changing sprintspeed to the normal movementspeed and stop stamina regeneration for the exhaustion time
+        else if (currentStamina <= 0 && exhaustedDebuff == false)
+        {
+            exhaustedDebuff = true;
+            StartCoroutine(ExhaustedTimer());
+        }
         RefreshUI();
     }
 
+    // Decrease Stamina by value
+    private void DecreaseStamina(float value)
+    {
+        currentStamina -= value;
+        if (currentStamina < 0)
+        {
+            currentStamina = 0;
+        }
+    }
+
+    // Increase Stamina bx value
+    private void IncreaseStamina(float value)
+    {
+        currentStamina += value;
+        if (currentStamina > _maxStamina)
+        {
+            currentStamina = _maxStamina;
+        }
+    }
+
+    // Refresh Userinterface
     private void RefreshUI()
     {
         staminaText.GetComponent<TextMeshProUGUI>().text = currentStamina.ToString();
     }
-    private IEnumerator Timer()
+
+    // Set sprintSpeed = moveSpeed to fake "not sprinting" then reset sprintSpeed
+    private IEnumerator ExhaustedTimer()
     {
-        yield return new WaitForSeconds(0.1f);
-        canJump = true;
+        float temp;
+        temp = _firstPersonController.SprintSpeed;
+        _firstPersonController.SprintSpeed = _firstPersonController.MoveSpeed;
+
+        yield return new WaitForSeconds(_exhaustDuration);
+
+        _firstPersonController.SprintSpeed = temp;
+        currentStamina = 1;
+        exhaustedDebuff = false;
     }
 
-    private IEnumerator DecreaseStaminaSprint()
+    // This Timer is here because I check if the conditions to make a jump are met, not if the jump was actually triggered (_firstPersonController.Grounded is triggered more than one frame)
+    private IEnumerator JumpTimer()
     {
-        currentStamina -= _decreaseAmountPerSecond;
-        yield return new WaitForSeconds(0.5f);
-        decreasingStamina = false;
+        yield return new WaitForSeconds(0.1f);
+        tickTimerRunning = false;
     }
-    private IEnumerator IncreaseStamina()
+
+    // Decrease or Increase stamina timed by a tickrate
+    private IEnumerator TickTimer()
     {
-        currentStamina += _increaseAmountPerSecond;
-        yield return new WaitForSeconds(0.5f);
-        increasingStamina = false;
+        yield return new WaitForSeconds(_tickTime);
+        tickTimerRunning = false;
     }
 }
